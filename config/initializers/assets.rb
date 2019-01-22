b12112438684f497654f0b69586c761681f4ba49
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2019-01-22 15:41:04 +0000

    Add GOV.UK frontend toolkit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,19 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('node_modules/govuk-frontend')
+Rails.application.config.assets.paths << Rails.root.join('node_modules/govuk-frontend/govuk')
+Rails.application.config.assets.paths << Rails.root.join('node_modules/govuk-frontend/govuk/assets')
+Rails.application.config.assets.paths << Rails.root.join('node_modules/govuk-frontend/govuk/assets/images')
+Rails.application.config.assets.paths << Rails.root.join('node_modules/govuk-frontend/govuk/assets/fonts')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w(
+    main-ie8
+    main
+    html5shiv.js
+    all.js
+)
